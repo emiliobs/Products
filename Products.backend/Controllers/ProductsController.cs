c30cc20bb8b6a24015eb6a80f05e521748fd38a6
emilio barrera <barrera_emilio@hotmail.com>
2018-06-07 12:12:14 +0100

    upload imagen in product controller

--- Products.backend/Controllers/ProductsController.cs
+++ Products.backend/Controllers/ProductsController.cs
@@ -6,6 +6,8 @@
     using System.Web.Mvc;
     using Products.Domain;
     using Products.backend.Models;
+    using Products.backend.Helpers;
+    using System;
 
     [Authorize(Users = "barrera_emilio@hotmail.com")]
     public class ProductsController : Controller
@@ -16,6 +18,7 @@
         public async Task<ActionResult> Index()
         {
             var products = db.Products.Include(p => p.Category);
+
             return View(await products.ToListAsync());
         }
 
@@ -38,6 +41,7 @@
         public ActionResult Create()
         {
             ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Description");
+
             return View();
         }
 
@@ -46,17 +50,48 @@
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "ProductId,CategoryId,Description,Price,IsActive,Stock,LastPurchase,Remarks")] Product product)
+        public async Task<ActionResult> Create(ProductView view)
         {
             if (ModelState.IsValid)
             {
+                var picture = string.Empty;
+                var folder = "~/Content/Images";
+
+                if (view.ImageFile != null)
+                {
+                    picture = FileHelper.UploadPhoto(view.ImageFile, folder);
+                    picture = $"{folder}/{picture}";
+                }
+
+                //Aqui convuerto la vista productView a product:
+                var product = ToProduct(view);
+                //aqui ya envio la imagen capturada:
+                product.Image = picture;
                 db.Products.Add(product);
                 await db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
 
-            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Description", product.CategoryId);
-            return View(product);
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Description", view.CategoryId);
+            return View(view);
+        }
+
+        private Product ToProduct(ProductView view)
+        {
+            return new Product()
+            {
+               Category = view.Category,
+               CategoryId = view.CategoryId,
+               Description = view.Description,
+               Image = view.Image,
+               IsActive = view.IsActive,
+               LastPurchase =view.LastPurchase,
+               Price = view.Price,
+               ProductId = view.ProductId,
+               Remarks = view.Remarks,
+               Stock = view.Stock,
+               
+            };
         }
 
         // GET: Products/Edit/5
@@ -66,13 +101,37 @@
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+
             Product product = await db.Products.FindAsync(id);
+
             if (product == null)
             {
                 return HttpNotFound();
             }
+
             ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Description", product.CategoryId);
-            return View(product);
+
+            var productView = ToView(product);
+
+            return View(productView);
+        }
+
+        private ProductView ToView(Product product)
+        {
+            return new ProductView()
+            {
+                Category = product.Category,
+                CategoryId = product.CategoryId,
+                Description = product.Description,
+                Image = product.Image,
+                IsActive = product.IsActive,
+                LastPurchase = product.LastPurchase,
+                Price = product.Price,
+                ProductId = product.ProductId,
+                Remarks = product.Remarks,
+                Stock = product.Stock,
+
+            };
         }
 
         // POST: Products/Edit/5
@@ -80,16 +139,31 @@
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "ProductId,CategoryId,Description,Price,IsActive,Stock,LastPurchase,Remarks")] Product product)
+        public async Task<ActionResult> Edit(ProductView view)
         {
             if (ModelState.IsValid)
             {
+
+                var picture = view.Image;
+                var folder = "~/Content/Images";
+
+                if (view.ImageFile != null)
+                {
+                    picture = FileHelper.UploadPhoto(view.ImageFile, folder);
+                    picture = $"{folder}/{picture}";
+                }
+
+                //Aqui convuerto la vista productView a product:
+                var product = ToProduct(view);
+                //aqui ya envio la imagen capturada:
+                product.Image = picture;
+
                 db.Entry(product).State = EntityState.Modified;
                 await db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
-            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Description", product.CategoryId);
-            return View(product);
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Description", view.CategoryId);
+            return View(view);
         }
 
         // GET: Products/Delete/5
---
+++ Products.backend/Helpers/FileHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Products.backend.Helpers
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Web;
+
+    public class FileHelper
+    {
+        public static string UploadPhoto(HttpPostedFileBase file, string folder)
+        {
+            var path = string.Empty;
+            var picture = string.Empty;
+
+            if (file != null)
+            {
+                picture = Path.GetFileName(file.FileName);
+                path = Path.Combine(HttpContext.Current.Server.MapPath(folder), picture);
+                file.SaveAs(path);
+            }
+
+            return picture;
+        }
+    }
+}
