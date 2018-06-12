eb5a86aaec0d2bc7d13e57e155f231c12bf63e0a
emilio barrera <barrera_emilio@hotmail.com>
2018-06-12 10:10:33 +0100

    Bication maps

--- Products/Products/Views/GeolocatorService.cs
+++ Products/Products/Views/GeolocatorService.cs
@@ -1,11 +1,57 @@
-﻿using System;
+﻿using Plugin.Geolocator;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Products.Views
 {
     public class GeolocatorService
     {
+        #region Properties
+        public double Latitude
+        {
+            get;
+            set;
+        }
 
+        public double Longitude
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public GeolocatorService()
+        {
+           
+        }
+
+        #endregion
+
+        #region Methods
+
+        public async Task GetLocation()
+        {
+            try
+            {
+                var locator = CrossGeolocator.Current;
+                locator.DesiredAccuracy = 50;
+                var location = await locator.GetPositionAsync();
+                Latitude = location.Latitude;
+                Longitude = location.Longitude;
+
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+            }
+
+        }
+
+        #endregion
     }
 }
--- Products/Products/Views/UbicationsView.xaml.cs
+++ Products/Products/Views/UbicationsView.xaml.cs
@@ -1,20 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-
-namespace Products.Views
+﻿namespace Products.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+    using Plugin.Geolocator.Abstractions;
+
+    using Xamarin.Forms;
+    using Xamarin.Forms.Maps;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class UbicationsView : ContentPage
 	{
-		public UbicationsView ()
-		{
-			InitializeComponent ();
-		}
-	}
+        #region services
+
+        GeolocatorService geolocatorService;
+
+        #endregion
+
+        #region Contructor
+        public UbicationsView()
+        {
+            InitializeComponent();
+            geolocatorService = new GeolocatorService();
+
+            MovepMapToCurrentPosition();
+        }
+
+
+        #endregion
+
+        #region Methods
+
+
+        private async void MovepMapToCurrentPosition()
+        {
+            await geolocatorService.GetLocation();
+            if (geolocatorService.Latitude != 0 || geolocatorService.Longitude != 0)
+            {
+                var position = new Xamarin.Forms.Maps.Position(geolocatorService.Latitude, geolocatorService.Longitude);
+                MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(position, Distance.FromKilometers(05)));
+
+            }
+        }
+
+        #endregion
+
+
+    }
 }
