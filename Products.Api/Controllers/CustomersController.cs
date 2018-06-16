e1d2aa010b567df1fd558b4036b3251434bd7ac6
emilio barrera <barrera_emilio@hotmail.com>
2018-06-16 13:53:44 +0100

    change passwird now is working

--- Products.Api/Controllers/CustomersController.cs
+++ Products.Api/Controllers/CustomersController.cs
@@ -1,26 +1,185 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
-using System.Web.Http;
-using System.Web.Http.Description;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-using Newtonsoft.Json.Linq;
-using Products.Api.Helpers;
-using Products.Api.Models;
-using Products.Domain;
-
-namespace Products.Api.Controllers
+﻿namespace Products.Api.Controllers
 {
+    
+    using Domain;
+    using Helpers;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Newtonsoft.Json.Linq;
+    using Products.Api.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+    using System.Data.Entity.Validation;
+    using System.Linq;
+    using System.Net;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+    using System.Web.Http.Description;
+
+    [RoutePrefix("api/Customers")]
     public class CustomersController : ApiController
     {
         private DataContext db = new DataContext();
+
+        [HttpPost]
+        [Authorize]
+        [Route("ChangePassword")]
+        public async Task<IHttpActionResult> ChangePassword(JObject form)
+        {
+            var email = string.Empty;
+            var currentPassword = string.Empty;
+            var newPassword = string.Empty;
+            dynamic jsonObject = form;
+
+            try
+            {
+                email = jsonObject.Email.Value;
+                currentPassword = jsonObject.CurrentPassword.Value;
+                newPassword = jsonObject.NewPassword.Value;
+            }
+            catch
+            {
+                return BadRequest("Incorrect call");
+            }
+
+            var userContext = new ApplicationDbContext();
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(userContext));
+            var userASP = userManager.FindByEmail(email);
+
+            if (userASP == null)
+            {
+                return NotFound();
+            }
+
+            var response = await userManager.ChangePasswordAsync(
+                userASP.Id,
+                currentPassword,
+                newPassword);
+            if (!response.Succeeded)
+            {
+                return BadRequest(response.Errors.FirstOrDefault());
+            }
+
+            return Ok(true);
+        }
+
+        [HttpPost]
+        [Route("PasswordRecovery")]
+        public async Task<IHttpActionResult> PasswordRecovery(JObject form)
+        {
+            try
+            {
+                var email = string.Empty;
+                dynamic jsonObject = form;
+
+                try
+                {
+                    email = jsonObject.Email.Value;
+                }
+                catch
+                {
+                    return BadRequest("Incorrect call.");
+                }
+
+                var customer = await db.Customers
+                    .Where(u => u.Email.ToLower() == email.ToLower())
+                    .FirstOrDefaultAsync();
+                if (customer == null)
+                {
+                    return NotFound();
+                }
+
+                var userContext = new ApplicationDbContext();
+                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(userContext));
+                var userASP = userManager.FindByEmail(email);
+                if (userASP == null)
+                {
+                    return NotFound();
+                }
+
+                var random = new Random();
+                var newPassword = string.Format("{0}", random.Next(100000, 999999));
+                var response1 = userManager.RemovePassword(userASP.Id);
+                var response2 = await userManager.AddPasswordAsync(userASP.Id, newPassword);
+                if (response2.Succeeded)
+                {
+                    var subject = "Products - Password Recovery";
+                    var body = string.Format(@"
+                        <h1>Products App - Password Recovery</h1>
+                        <p>Your new password is: <strong>{0}</strong></p>
+                        <p>Please, don't forget change it for one easy remember for you.",
+                        newPassword);
+
+                    await MailHelper.SendMail(email, subject, body);
+                    return Ok(true);
+                }
+
+                return BadRequest("The password can't be changed.");
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        //[HttpPost]
+        //[Route("LoginFacebook")]
+        //public async Task<IHttpActionResult> LoginFacebook(FacebookResponse profile)
+        //{
+        //    try
+        //    {
+        //        var customer = await db.Customers
+        //            .Where(c => c.Email == profile.Id)
+        //            .FirstOrDefaultAsync();
+        //        if (customer == null)
+        //        {
+        //            customer = new Customer
+        //            {
+        //                Email = profile.Id,
+        //                FirstName = profile.FirstName,
+        //                LastName = profile.LastName,
+        //                CustomerType = 2,
+        //                Password = profile.Id,
+        //            };
+
+        //            db.Customers.Add(customer);
+        //            CreateUserASP(profile.Id, profile.Id);
+        //        }
+        //        else
+        //        {
+        //            customer.FirstName = profile.FirstName;
+        //            customer.LastName = profile.LastName;
+        //            customer.Password = profile.Id;
+        //            db.Entry(customer).State = EntityState.Modified;
+        //        }
+
+        //        await db.SaveChangesAsync();
+
+        //        return Ok(true);
+        //    }
+        //    catch (DbEntityValidationException e)
+        //    {
+        //        var message = string.Empty;
+        //        foreach (var eve in e.EntityValidationErrors)
+        //        {
+        //            message = string.Format("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+        //                eve.Entry.Entity.GetType().Name, eve.Entry.State);
+        //            foreach (var ve in eve.ValidationErrors)
+        //            {
+        //                message += string.Format("\n- Property: \"{0}\", Error: \"{1}\"",
+        //                    ve.PropertyName, ve.ErrorMessage);
+        //            }
+        //        }
+
+        //        return BadRequest(message);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return BadRequest(ex.Message);
+        //    }
+        //}
 
         // GET: api/Customers
         public IQueryable<Customer> GetCustomers()
@@ -86,16 +245,13 @@ namespace Products.Api.Controllers
             }
 
             db.Customers.Add(customer);
-
             try
             {
                 CreateUserASP(customer.Email, customer.Password);
                 await db.SaveChangesAsync();
-
             }
             catch (Exception ex)
             {
-
                 if (ex.InnerException != null &&
                     ex.InnerException.InnerException != null &&
                     ex.InnerException.InnerException.Message.Contains("Index"))
@@ -117,11 +273,10 @@ namespace Products.Api.Controllers
             var userManager = new UserManager<ApplicationUser>(
                 new UserStore<ApplicationUser>(userContext));
 
-            var userASP = new ApplicationUser()
+            var userASP = new ApplicationUser
             {
                 Email = email,
                 UserName = email,
-
             };
 
             var result = userManager.Create(userASP, password);
@@ -129,6 +284,7 @@ namespace Products.Api.Controllers
             {
                 return true;
             }
+
             return false;
         }
 
@@ -147,110 +303,6 @@ namespace Products.Api.Controllers
 
             return Ok(customer);
         }
-
-        [HttpPost]
-        [Authorize]
-        [Route("ChangePassword")]
-        public async Task<IHttpActionResult> ChangePassword(JObject form)
-        {
-            var email = string.Empty;
-            var currentPassword = string.Empty;
-            var newPassword = string.Empty;
-            dynamic jsonObject = form;
-
-            try
-            {
-                email = jsonObject.Email.Value;
-                currentPassword = jsonObject.CurrentPassword.Value;
-                newPassword = jsonObject.NewPassword.Value;
-            }
-            catch
-            {
-                return BadRequest("Incorrect call");
-            }
-
-            var userContext = new ApplicationDbContext();
-            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(userContext));
-            var userASP = userManager.FindByEmail(email);
-
-            if (userASP == null)
-            {
-                return NotFound();
-            }
-
-            var response = await userManager.ChangePasswordAsync(
-                userASP.Id,
-                currentPassword,
-                newPassword);
-            if (!response.Succeeded)
-            {
-                return BadRequest(response.Errors.FirstOrDefault());
-            }
-
-            return Ok(true);
-        }
-
-
-        [HttpPost]
-        [Route("PasswordRecovery")]
-        public async Task<IHttpActionResult> PasswordRecovery(JObject form)
-        {
-            try
-            {
-                var email = string.Empty;
-                dynamic jsonObject = form;
-
-                try
-                {
-                    email = jsonObject.Email.Value;
-                }
-                catch
-                {
-                    return BadRequest("Incorrect call.");
-                }
-
-                var customer = await db.Customers
-                    .Where(u => u.Email.ToLower() == email.ToLower())
-                    .FirstOrDefaultAsync();
-                if (customer == null)
-                {
-                    return NotFound();
-                }
-
-                var userContext = new ApplicationDbContext();
-                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(userContext));
-                var userASP = userManager.FindByEmail(email);
-                if (userASP == null)
-                {
-                    return NotFound();
-                }
-
-                var random = new Random();
-                var newPassword = string.Format("{0}", random.Next(100000, 999999));
-                var response1 = userManager.RemovePassword(userASP.Id);
-                var response2 = await userManager.AddPasswordAsync(userASP.Id, newPassword);
-                if (response2.Succeeded)
-                {
-                    var subject = "Products - Password Recovery";
-                    var body = string.Format(@"
-                        <h1>Products App - Password Recovery</h1>
-                        <p>Your new password is: <strong>{0}</strong></p>
-                        <p>Please, don't forget change it for one easy remember for you.",
-                        newPassword);
-
-                    await MailHelper.SendMail(email, subject, body);
-                    return Ok(true);
-                }
-
-                return BadRequest("The password can't be changed.");
-
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-        }
-
 
         protected override void Dispose(bool disposing)
         {
